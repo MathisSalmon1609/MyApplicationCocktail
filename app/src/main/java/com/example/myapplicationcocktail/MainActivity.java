695a2559380245ec0aefa52fef7915c8de4d1712
Salmon Mathis <mathis.salmon@etu.univ-orleans.fr>
2019-11-22 01:44:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationcocktail/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplicationcocktail;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.)
+
+
+        super.onBackPressed();
+    }
+}
